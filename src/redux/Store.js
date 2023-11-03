80f292cc4cb9f76be025a02836114e1e697c3bd2
Mouhamed Doumbia <doumbiasoft@gmail.com>
2023-11-03 00:32:21 -0400

    fix date format

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,14 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
 import RootReducers from './Rootreducers';
 
-// eslint-disable-next-line import/prefer-default-export
 export function configureStore(InitialState) {
   const Store = createStore(
     RootReducers,
     InitialState,
-    composeWithDevTools(applyMiddleware(thunk)),
   );
   return Store;
 }
